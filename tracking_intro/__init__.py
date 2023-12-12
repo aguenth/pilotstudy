5a950f81746125c7717b6069200bd01cfbf1606a
aguenth <anne.guenther5@t-online.de>
2023-12-12 11:01:57 +0100

    Questions added

--- tracking_intro/__init__.py
+++ tracking_intro/__init__.py
@@ -41,50 +41,6 @@ class Player(BasePlayer):
         widget=widgets.RadioSelect,
     )
 
-    own_car = models.StringField(
-        label='Does your household own a car (or lease a car)?',
-        choices=[['No', 'No'],
-                 ['gasoline', 'Yes, a gasoline car'],
-                 ['hybrid', 'Yes, a hybrid electric vehicle'],
-                 ['ev', 'Yes, an electric vehicle']],
-        widget=widgets.RadioSelect,
-    )
-
-    car_model = models.StringField(
-        label='What size car does your household own / lease?',
-        choices=[['Small', 'Small car (e.g., Toyota Yaris, Fiat 500, Dacia Sandero)'],
-                 ['Medium', 'Medium sized car (e.g., VW Golf, Audi A6)'],
-                 ['Luxury', 'Luxury car / Sports car / SUV (e.g., Range Rover, Porsche, Mercedes-Benz S-Class)']],
-        widget=widgets.RadioSelect,
-        blank=True,
-    )
-
-    car_number = models.StringField(
-        label='How many cars do you own?',
-        choices=[['1', 'One'],
-                 ['2', 'Two'],
-                 ['3', 'Three']],
-        widget=widgets.RadioSelect,
-        blank=True,
-    )
-
-    car_age = models.StringField(
-        label='When have you bought the main car you are using?',
-        choices=[['Less than 5 years ago', 'Less than 5 years ago'],
-                 ['Less than 10 years ago', 'Less than 10 years ago'],
-                 ['More than 10 years ago', 'More than 10 years ago']],
-        widget=widgets.RadioSelect,
-        blank=True,
-    )
-
-    car = models.StringField(
-        label='Whether or not you currently own a car, envisioning a scenario where you are purchasing a new one, what size of car would you find most appealing for your daily journeys?',
-        choices=[['Small', 'Small car (e.g., Toyota Yaris, Fiat 500, Dacia Sandero)'],
-                 ['Medium', 'Medium sized car (e.g., VW Golf, Audi A6)'],
-                 ['Luxury', 'Luxury car / Sports car / SUV (e.g., Range Rover, Porsche, Mercedes-Benz S-Class)']],
-        widget=widgets.RadioSelect,
-    )
-
     drivers_license = models.StringField(
         label="Do you currently hold a valid driver's license?",
         choices=[['Yes', 'Yes'],
@@ -92,7 +48,84 @@ class Player(BasePlayer):
                  ['Process', 'In the process of obtaining a license']],
         widget=widgets.RadioSelect,
     )
+    own_car = models.StringField(
+        label='Does your household own a car (or lease a car)?',
+        choices=[['No', 'No'],
+                 ['Yes', 'Yes']],
+        widget=widgets.RadioSelect,
+    )
+    car_type = models.StringField(
+        label='What kind of car is your main car you use for daily trips?',
+        choices=[['gasoline', 'Gasoline car'],
+                 ['ev', 'Electric car'],
+                 ['hev', 'Hybrid car']],
+        widget=widgets.RadioSelect,
+        blank=True,
+    )
+    car_model = models.StringField(
+        label='What size is that car?',
+        choices=[['Small', 'Small car (e.g., Toyota Yaris, Fiat 500, Dacia Sandero)'],
+                 ['Medium', 'Medium sized car (e.g., VW Golf, Audi A6)'],
+                 ['Luxury', 'Luxury car / Sports car / SUV (e.g., Range Rover, Porsche, Mercedes-Benz S-Class)']],
+        widget=widgets.RadioSelect,
+        blank=True,
+    )
+    car_number = models.StringField(
+        label='How many cars does your household own in total?',
+        choices=[['1', '1 car'],
+                 ['2', '2 cars'],
+                 ['3', '3 cars'],
+                 ['4', 'More than 3 cars']],
+        widget=widgets.RadioSelect,
+        blank=True,
+    )
+    car_age = models.IntegerField(label='How old is your main car (in years)?', min=0, max=30, blank=True)
+    car_replace = models.StringField(
+        label='How often do you replace your car?',
+        choices=[['every year', 'Every year'],
+                 ['every four years', 'Every 4 years'],
+                 ['every eight years', 'Every 8 years'],
+                 ['every twelve years', 'Every 12 years']],
+        widget=widgets.RadioSelect,
+        blank=True,
+    )
+    car = models.StringField(
+        label='Whether or not you currently own a car, envisioning a scenario where you are purchasing a new one, '
+              'what size of car would you find most appealing for your daily journeys?',
+        choices=[['Small', 'Small car (e.g., Toyota Yaris, Fiat 500, Dacia Sandero)'],
+                 ['Medium', 'Medium sized car (e.g., VW Golf, Audi A6)'],
+                 ['Luxury', 'Luxury car / Sports car / SUV (e.g., Range Rover, Porsche, Mercedes-Benz S-Class)']],
+        widget=widgets.RadioSelect,
+    )
+    km_week = models.IntegerField(label='How many miles do you drive in a typical week?', min=0, max=500, blank=True)
 
+    wom_owner = models.StringField(
+        label='If someone you know asks you for your opinion on your electric car, do you give... ',
+        choices=[['neutral', 'a neutral response'],
+                 ['positive', 'a positive response'],
+                 ['negative', 'a negative response'],
+                 ['mixed', 'a mixed response']],
+        widget=widgets.RadioSelect,
+        blank=True,
+    )
+    wom_number = models.IntegerField(label='How many people do you know who own an electric vehicle?',
+                                     min=0, max=50)
+    wom_positive = models.StringField(
+        label='In the past month, how often have you heard <b>positive</b> comments about electric vehicles?',
+        choices=[['every day', 'More than once a week'],
+                 ['once week', 'Once a week'],
+                 ['once month', 'Once a month'],
+                 ['never', 'Never']],
+        widget=widgets.RadioSelect,
+        )
+    wom_negative = models.StringField(
+        label='In the past month, how often have you heard <b>negative</b> comments about electric vehicles?',
+        choices=[['every day', 'More than once a week'],
+                 ['once week', 'Once a week'],
+                 ['once month', 'Once a month'],
+                 ['never', 'Never']],
+        widget=widgets.RadioSelect,
+        )
     # Consent fields
     dataScience = models.BooleanField(initial=False)
     dataTeach = models.BooleanField(initial=False)
@@ -126,7 +159,8 @@ class HoverEvent(models.ExtraModel):
 
 
 def custom_export(players):
-    yield ["session", "participant_code", "round_number", "id_in_group", "element_id", "enter_time", "leave_time", "duration", "attributeType", "attributeValue", "buyEV"]
+    yield ["session", "participant_code", "round_number", "id_in_group", "element_id", "enter_time", "leave_time",
+           "duration", "attributeType", "attributeValue", "buyEV"]
     for player in players:
         for e in HoverEvent.filter(player=player):
             yield [
@@ -147,6 +181,11 @@ def custom_export(players):
 class introduction_consent(Page):
     form_model = 'player'
     form_fields = ['dataScience', 'dataTeach']
+    @staticmethod
+    def vars_for_template(player: Player):
+        return {
+            "particpantlabel": player.participant.label,
+        }  # add ?participant_label={{%PROLIFIC_PID%}} to link on prolific
 
 
 # Demographics Page class
@@ -157,7 +196,8 @@ class Demographics(Page):
 
 class Car_questions(Page):
     form_model = 'player'
-    form_fields = ['drivers_license', 'own_car', 'car_model', 'car', 'car_number', 'car_age']
+    form_fields = ['drivers_license', 'own_car', 'car_model', 'car', 'car_type', 'car_number', 'car_age', 'car_replace',
+                   'km_week']
 
     def before_next_page(player: Player, timeout_happened):
         # Store car value in participant.vars
@@ -165,6 +205,11 @@ class Car_questions(Page):
 
         # Add console log statement
         print(f"DEBUG: Car value stored - {player.participant.vars['car']}")
+
+
+class WoM(Page):
+    form_model = 'player'
+    form_fields = ['wom_owner', 'wom_number', 'wom_positive', 'wom_negative']
 
 
 class instructions(Page):
@@ -185,6 +230,7 @@ page_sequence = [
     introduction_consent,
     Demographics,
     Car_questions,
+    WoM,
     instructions,
     Tracker_demo,
     practice_completed_template
